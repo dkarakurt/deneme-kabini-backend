2416eec5d2c2332d583237be6d011bad24e30db3
dkarakurt <43344585+dkarakurt@users.noreply.github.com>
2025-11-29 20:48:52 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,22 @@
+export const config = {
+  api: {
+    bodyParser: true,
+  },
+};
+
 export default async function handler(req, res) {
+  // CORS - HER DURUMDA
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Max-Age', '86400');
+
+  // OPTIONS request için hemen dön
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
+  export default async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
